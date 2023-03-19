21733698ead6029763b1672a4c09f52b32745bd9
Tung Nguyen <Nguyen.Tung@outlook.de>
2023-03-20 00:01:44 +0100

    restored settings ui

--- lib/Screens/Mehr/settings.dart
+++ lib/Screens/Mehr/settings.dart
@@ -42,9 +42,11 @@ class SectionSettings extends StatelessWidget {
 class _settingsStartState extends State<ScreenSettings>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-
+  late String _themeModeValue;
+  bool isPressed = false;
   @override
   void initState() {
+    _themeModeValue = "system";
     _controller = AnimationController(
         value: 0.0,
         duration: const Duration(milliseconds: 500),
@@ -62,35 +64,9 @@ class _settingsStartState extends State<ScreenSettings>
     super.dispose();
   }
 
-  bool get _isAnimationRunningForwardsOrComplete {
-    switch (_controller.status) {
-      case AnimationStatus.forward:
-      case AnimationStatus.completed:
-        return true;
-      case AnimationStatus.reverse:
-      case AnimationStatus.dismissed:
-        return false;
-    }
-  }
-
-  String _themeModeValue = "system";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: AnimatedBuilder(
-        animation: _controller,
-        builder: (BuildContext context, Widget? child) {
-          return FadeScaleTransition(animation: _controller, child: child);
-        },
-        child: Visibility(
-          visible: _controller.status != AnimationStatus.dismissed,
-          child: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {},
-          ),
-        ),
-      ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -117,111 +93,113 @@ class _settingsStartState extends State<ScreenSettings>
                   ],
                 )),
             Expanded(
-                child: SettingsList(physics: ScrollPhysics(), sections: [
-              CustomSettingsSection(
-                  child: user == null || session == null
-                      ? SectionProfileGast()
-                      : SectionProfile()),
-              CustomSettingsSection(
-                  child: SettingsSection(
-                title: Text("Einstellungen"),
-                tiles: [
-                  SettingsTile(
-                    title:
-                        // Text("Dunkelmodus"),
-                        Container(
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Theme"),
-                          Row(
-                            children: [
-                              Radio(
-                                  value: "system",
-                                  groupValue: _themeModeValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      Get.changeThemeMode(ThemeMode.system);
-                                      _changeTheme(value!);
-                                    });
-                                  }),
-                              Text("System")
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Radio(
-                                  value: "light",
-                                  groupValue: _themeModeValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      Get.changeThemeMode(ThemeMode.light);
-                                      _changeTheme(value!);
-                                    });
-                                  }),
-                              Text("Hell")
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Radio(
-                                  value: "dark",
-                                  groupValue: _themeModeValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      Get.changeThemeMode(ThemeMode.dark);
-                                      _changeTheme(value!);
-                                    });
-                                  }),
-                              Text("Dunkel")
-                            ],
-                          ),
-                        ],
+              child: SettingsList(physics: ScrollPhysics(), sections: [
+                CustomSettingsSection(
+                    child: user == null || session == null
+                        ? SectionProfileGast()
+                        : SectionProfile()),
+                CustomSettingsSection(
+                    child: SettingsSection(
+                  title: Text("Einstellungen"),
+                  tiles: [
+                    SettingsTile(
+                      title:
+                          // Text("Dunkelmodus"),
+                          Container(
+                        padding: EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Theme"),
+                            Row(
+                              children: [
+                                Radio(
+                                    value: "system",
+                                    groupValue: _themeModeValue,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        Get.changeThemeMode(ThemeMode.system);
+                                        _changeTheme(value!);
+                                      });
+                                    }),
+                                Text("System")
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio(
+                                    value: "light",
+                                    groupValue: _themeModeValue,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        Get.changeThemeMode(ThemeMode.light);
+                                        _changeTheme(value!);
+                                      });
+                                    }),
+                                Text("Hell")
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio(
+                                    value: "dark",
+                                    groupValue: _themeModeValue,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        Get.changeThemeMode(ThemeMode.dark);
+                                        _changeTheme(value!);
+                                      });
+                                    }),
+                                Text("Dunkel")
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
 
-                    // Switch(
-                    //   value: Get.isDarkMode,
-                    //   onChanged: (value) {
-                    //     setState(() {
-                    //       Get.changeThemeMode(Get.isDarkMode
-                    //           ? ThemeMode.light
-                    //           : ThemeMode.dark);
-                    //     });
-                    //   },
-                    // ),
-                  ),
-                  SettingsTile(title: Text("allo"))
-                ],
-              )),
-              CustomSettingsSection(child: SectionSupport()),
-              CustomSettingsSection(
-                  child: SettingsSection(
-                title: Text("Über uns"),
-                tiles: [
-                  SettingsTile(title: Text("Allgemeine Geschäftsbedingungen")),
-                  SettingsTile(title: Text("Datenschutzhinweise")),
-                  SettingsTile(title: Text("Impressum")),
-                  SettingsTile(
-                    title: Text("App-Informationen"),
-                    onPressed: (context) {
-                      showModal(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text("App-Informationen"),
-                              content: Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text("App-Version: " "1"),
-                              ),
-                            );
-                          });
-                    },
-                  )
-                ],
-              )),
-            ]))
+                      // Switch(
+                      //   value: Get.isDarkMode,
+                      //   onChanged: (value) {
+                      //     setState(() {
+                      //       Get.changeThemeMode(Get.isDarkMode
+                      //           ? ThemeMode.light
+                      //           : ThemeMode.dark);
+                      //     });
+                      //   },
+                      // ),
+                    ),
+                    SettingsTile(title: Text("allo"))
+                  ],
+                )),
+                // CustomSettingsSection(child: SectionSupport()),
+                CustomSettingsSection(
+                    child: SettingsSection(
+                  title: Text("Über uns"),
+                  tiles: [
+                    SettingsTile(
+                        title: Text("Allgemeine Geschäftsbedingungen")),
+                    SettingsTile(title: Text("Datenschutzhinweise")),
+                    SettingsTile(title: Text("Impressum")),
+                    SettingsTile(
+                      title: Text("App-Informationen"),
+                      onPressed: (context) {
+                        showModal(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("App-Informationen"),
+                                content: Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text("App-Version: " "1"),
+                                ),
+                              );
+                            });
+                      },
+                    )
+                  ],
+                )),
+              ]),
+            )
           ],
         ),
       ),
@@ -235,22 +213,50 @@ class _settingsStartState extends State<ScreenSettings>
   }
 }
 
-class SectionSupport extends StatelessWidget {
-  const SectionSupport({
-    super.key,
-  });
+// class SectionSupport extends StatelessWidget {
+//   const SectionSupport({
+//     super.key,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return SettingsSection(
-      title: Text("Support"),
-      tiles: [
-        SettingsTile(title: Text("Hilfe")),
-        SettingsTile(title: Text("Kontakt"))
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsetsDirectional.only(
+//         start: 18,
+//         bottom: 5 * scaleFactor,
+//       ),
+//       child: DefaultTextStyle(
+//         style: TextStyle(
+//           color: theme.themeData.titleTextColor,
+//           fontSize: 13,
+//         ),
+//         child: title!,
+//       ),
+//     );
+
+// Container(
+//   color: themeData.settingsListBackground,
+//   width: MediaQuery.of(context).size.width,
+//   alignment: Alignment.center,
+//   child: ConstrainedBox(
+//     constraints: BoxConstraints(maxWidth: 810),
+//     child: SettingsTheme(
+//       themeData: themeData,
+//       platform: platform,
+//       child: ListView.builder(
+//         physics: physics,
+//         shrinkWrap: shrinkWrap,
+//         itemCount: sections.length,
+//         padding: contentPadding ?? calculateDefaultPadding(platform),
+//         itemBuilder: (BuildContext context, int index) {
+//           return sections[index];
+//         },
+//       ),
+//     ),
+//   ),
+// );
+//   }
+// }
 
 class SectionProfile extends StatelessWidget {
   const SectionProfile({
